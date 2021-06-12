07c18e82bb1b461ae3afbf4b38c6904f1df1decc
Ronak.D <ronak.dave@prospection.com.au>
2021-06-12 18:01:28 +1000

    +game service +tests

--- src/main/java/io/ronakdave/game/service/GameService.java
+++ src/main/java/io/ronakdave/game/service/GameService.java
@@ -1,5 +1,34 @@
 package io.ronakdave.game.service;
 
+import java.util.NoSuchElementException;
+
+import io.ronakdave.game.engine.RockPaperScissorsGameEngine;
+import io.ronakdave.game.model.GameResult;
+import io.ronakdave.game.model.GameResultSummary;
+import io.ronakdave.game.model.Player;
+import io.ronakdave.game.model.Shape;
+import io.ronakdave.game.repositories.PlayerRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public class GameService {
+    private final RockPaperScissorsGameEngine gameEngine;
+    private final PlayerRepository playerRepository;
+
+    public GameResultSummary playRound(Shape playerShape, Long playerId) throws NoSuchElementException {
+        // Pass shape to engine
+        // get result
+        GameResult result = gameEngine.runEngine(playerShape);
+        // Get the player from repository
+        Player currentPlayer = playerRepository.findById(playerId).orElseThrow();
+        // calc player win, loss, played metrics
+        // create result object
+        // return result
+        return GameResultSummary.builder()
+            .result(result)
+            .player(currentPlayer)
+            .build();
+    }
+
     
 }
--- src/test/java/io/ronakdave/game/service/GameServiceTest.java
+++ src/test/java/io/ronakdave/game/service/GameServiceTest.java
@@ -1,7 +1,62 @@
 package io.ronakdave.game.service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import io.ronakdave.game.engine.RockPaperScissorsGameEngine;
+import io.ronakdave.game.model.GameResult;
+import io.ronakdave.game.model.GameResultSummary;
+import io.ronakdave.game.model.Player;
+import io.ronakdave.game.model.Shape;
+import io.ronakdave.game.repositories.PlayerRepository;
+
+@ExtendWith(MockitoExtension.class)
 public class GameServiceTest {
 
-    private GameService gameService = new GameService();
+    @Mock
+    private RockPaperScissorsGameEngine gameEngine;
+
+    @Mock
+    private PlayerRepository playerRepository;
+
+    @InjectMocks
+    private GameService gameService;
+
+    @BeforeEach
+    public void setup() {
+        when(gameEngine.runEngine(Shape.ROCK)).thenReturn(GameResult.WIN);
+        when(playerRepository.findById(1L)).thenReturn(
+            Optional.of(
+                Player.builder()
+                    .name("test")
+                    .totalPlayed(0)
+                    .lost(0)
+                    .won(0)
+                    .build()
+            )
+        );
+    }
+
+    @Test
+    public void whenGameServiceSubmitRound_thenRunEngineToProduceValidResult() {
+        GameResultSummary summary = gameService.playRound(Shape.ROCK, 1L);
+
+        // summary should not be null
+        assertNotNull(summary);
+        // result should not be null
+        assertNotNull(summary.getResult());
+        // Player should not be null
+        assertNotNull(summary.getPlayer());
+    }
     
 }

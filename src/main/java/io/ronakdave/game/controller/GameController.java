e166986e2f71a32a808db9ec5301e132686daee8
Ronak.D <ronak.dave@prospection.com.au>
2021-06-14 08:58:11 +1000

    +api base path -startGame endpoint

--- src/main/java/io/ronakdave/game/controller/GameController.java
+++ src/main/java/io/ronakdave/game/controller/GameController.java
@@ -2,28 +2,22 @@ package io.ronakdave.game.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.Assert;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.ronakdave.game.model.GameResultSummary;
 import io.ronakdave.game.model.Shape;
-import io.ronakdave.game.response.GameApiResponse;
 import io.ronakdave.game.response.GameResultResponse;
 import io.ronakdave.game.service.GameService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/game")
 public class GameController {
     private final GameService gameService;
-
-    @GetMapping("/startGame")
-    public ResponseEntity<GameApiResponse> startGame() {
-        // To start playing user must hit the endpoint first
-        return ResponseEntity.ok(new GameApiResponse("game started"));
-    }
 
     @PostMapping("/submit/{playerId}/{playerShape}")
     public ResponseEntity<GameResultResponse> submit(@PathVariable("playerId") int playerId, @PathVariable("playerShape") String playerShape) {

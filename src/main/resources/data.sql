c22c924af637ad24380ba05e124df2826f3bf8a7
Ronak.D <ronak.dave@prospection.com.au>
2021-06-14 11:44:28 +1000

    +users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
 INSERT INTO `players` (`id`, `games_draw`, `games_lost`, `games_played`, `games_won`, `password`, `roles`, `username`)
-VALUES ('1', '0', '0', '0', '0', '$2y$12$Um9X1Fl1NFtq1o4.W5YZVOl5Tt/WQ51TSnVbuNc8ngJS03YTqL9p.', 'ADMIN', 'admin');
+VALUES ('1', '0', '0', '0', '0', '$2y$12$nvtKiCuM9RbUBBvPD75OwueXF3cqIq6W4w7X3nXjtpaBKUbcmTNDu', 'ROLE_ADMIN', 'admin');
+-- admin123
 
 INSERT INTO `players` (`id`, `games_draw`, `games_lost`, `games_played`, `games_won`, `password`, `roles`, `username`)
-VALUES ('2', '0', '0', '0', '0', '$2y$12$o38cZbu4p8E5vt6YYm9yneY4E/nUunQCDGDetkjmUA4/ttWNfPsn. ', 'USER', 'user');
+VALUES ('2', '0', '0', '0', '0', '$2y$12$o38cZbu4p8E5vt6YYm9yneY4E/nUunQCDGDetkjmUA4/ttWNfPsn.', 'ROLE_USER', 'user');
+-- pass1

a630b5e0307f270eb42e18d6dea434da786a4477
Ronak.D <ronak.dave@prospection.com.au>
2021-06-14 11:00:36 +1000

    +load players on start

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `players` (`id`, `games_draw`, `games_lost`, `games_played`, `games_won`, `password`, `roles`, `username`)
+VALUES ('1', '0', '0', '0', '0', '$2y$12$Um9X1Fl1NFtq1o4.W5YZVOl5Tt/WQ51TSnVbuNc8ngJS03YTqL9p.', 'ADMIN', 'admin');
+
+INSERT INTO `players` (`id`, `games_draw`, `games_lost`, `games_played`, `games_won`, `password`, `roles`, `username`)
+VALUES ('2', '0', '0', '0', '0', '$2y$12$o38cZbu4p8E5vt6YYm9yneY4E/nUunQCDGDetkjmUA4/ttWNfPsn. ', 'USER', 'user');

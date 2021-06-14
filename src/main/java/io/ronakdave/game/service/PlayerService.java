b0d18bacaaa45c66b120d82e2e083f04884218d4
Ronak.D <ronak.dave@prospection.com.au>
2021-06-14 12:13:33 +1000

    +only get players

--- src/main/java/io/ronakdave/game/service/PlayerService.java
+++ src/main/java/io/ronakdave/game/service/PlayerService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 
 import io.ronakdave.game.model.Player;
 import io.ronakdave.game.repository.PlayerRepository;
-import io.ronakdave.game.request.UserAuthRequest;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,22 +13,6 @@ import lombok.RequiredArgsConstructor;
 public class PlayerService {
 
     private final PlayerRepository playerRepository;
-
-    public void savePlayer(UserAuthRequest userDetails) {
-
-        Player newPlayer = Player.builder()
-            .username(userDetails.getUsername())
-            // Encode this password
-            .password(userDetails.getPassword())
-            .build();
-        
-        playerRepository.save(newPlayer);
-
-    }
-
-    public boolean usernameTaken(String username) {
-        return playerRepository.existsByUsername(username);
-    }
 
     public List<Player> getAllPlayers() {
         return playerRepository.findAll();

de80352d3ffc584f27f476a00b5f82e6afa1498c
Ronak.D <ronak.dave@prospection.com.au>
2021-06-14 11:44:01 +1000

    +always use uppercase

--- src/main/java/io/ronakdave/game/model/Shape.java
+++ src/main/java/io/ronakdave/game/model/Shape.java
@@ -3,5 +3,9 @@ package io.ronakdave.game.model;
 public enum Shape {
     ROCK,
     PAPER,
-    SCISSORS
+    SCISSORS;
+
+    public static Shape getShape(String shape) {
+        return Shape.valueOf(shape.toUpperCase());
+    }
 }

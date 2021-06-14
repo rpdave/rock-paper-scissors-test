fbe84f8e34f88b39a97a112c9251bc9dcb64fff3
Ronak.D <ronak.dave@prospection.com.au>
2021-06-14 11:43:42 +1000

    +ignore sensitive props

--- src/main/java/io/ronakdave/game/model/Player.java
+++ src/main/java/io/ronakdave/game/model/Player.java
@@ -6,6 +6,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,14 +23,21 @@ public class Player {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Long id;
     private String username;
+    @JsonIgnore
     private String password;
+    @JsonIgnore
     private String roles;
     
     // Player Stats
+    @JsonIgnore
     private int gamesPlayed;
+    @JsonIgnore
     private int gamesWon;
+    @JsonIgnore
     private int gamesLost;
+    @JsonIgnore
     private int gamesDraw;
 }

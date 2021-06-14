c5953b233f76e460cb9e6d0bf8c27553eb0fadd5
Ronak.D <ronak.dave@prospection.com.au>
2021-06-14 18:13:43 +1000

    +test user +test admin

--- src/test/java/io/ronakdave/game/controller/GameControllerTest.java
+++ src/test/java/io/ronakdave/game/controller/GameControllerTest.java
@@ -2,7 +2,6 @@ package io.ronakdave.game.controller;
 
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,36 +10,62 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import io.ronakdave.game.security.CustomUserDetailsService;
 import io.ronakdave.game.service.GameService;
 
 @WebMvcTest(GameController.class)
 public class GameControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    MockMvc mvc;
 
     @MockBean
-    private GameService gameService;
+    GameService gameService;
+
+    @MockBean
+    Authentication authentication;
+
+    @MockBean
+    CustomUserDetailsService userDetailsService;
 
     @Test
-    public void whenPlayerIdIsInvalid_thenReturnBadRequest() throws Exception {
+    public void whenNoValidUser_thenReturnForbidden() throws Exception {
         mvc
-            .perform(post("/submit/0/rock").accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
+            .perform(get("/api/game/submit/rock"))
             .andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(content().string(containsString("Invalid player id")));
+            .andExpect(status().isUnauthorized());
     }
 
     @Test
+    @WithMockUser(username = "user", roles = "USER")
+    public void whenSubmitValidShape_thenReturnGameSummary() throws Exception {
+        mvc
+            .perform(get("/api/game/submit/rock"))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "user", roles = "USER")
     public void whenShapeIsInvlaid_thenReturnBadRequest() throws Exception {
         mvc
-            .perform(post("/submit/1/null"))
+            .perform(get("/api/game/submit/null"))
             .andDo(print())
             .andExpect(status().isBadRequest())
-            .andExpect(content().string(containsString("Invalid shape provided")));
+            .andExpect(content().string(containsString("Invalid shape:")));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void whenAdminTryToPlay_thenReturnFobidden() throws Exception {
+        mvc
+            .perform(get("/api/game/submit/rock"))
+            .andDo(print())
+            .andExpect(status().isForbidden());
     }
 
 }
